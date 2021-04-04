b622bbb207d5e83aaa07262d9c5b6ab9f98c61be
dgoncharik <dgoncharik27@gmail.com>
2021-04-04 19:57:30 +0300

    server.js -> port = process.env.PORT || 3001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./public/db.json');
 const middlewares = jsonServer.defaults({ static: './build' });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // server.use('/', express.static(path.join(__dirname, 'build')))
 
